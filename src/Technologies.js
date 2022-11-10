156262fd623503c585649cd393b419ea20ebb387
Viktor <364185@protonmail.com>
2022-11-10 17:19:09 +0400

    L11

---
+++ src/Technologies.js
@@ -0,0 +1,14 @@
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Technologies;
