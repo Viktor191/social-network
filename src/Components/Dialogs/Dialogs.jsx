d8a1b478fa31cc307a816c7b5229892299c45662
Viktor <364185@protonmail.com>
2022-11-16 19:27:39 +0400

    L26

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,47 +1,31 @@
 import classes from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
-
-
-const DialogItem = (props) => {
-    let path = '/dialogs/' + props.id;
-    return (
-        <div className={classes.dialog + ' ' + classes.active}>
-            <NavLink to={path}>{props.name}</NavLink>
-        </div>
-    )
-}
-const Message = (props) => {
-    return (
-        <div className={classes.dialog}>{props.message}</div>
-    )
-}
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
 
 const Dialogs = (props) => {
-    let dialogsData = [
+    let dialogs = [
         {id:1, name:'Dimych'},
         {id:2, name:'Andrey'},
         {id:3, name:'Sveta'},
         {id:4, name:'Sasha'},
         {id:5, name:'Viktor'}
     ]
-    let messagesData = [
+    let dialogsElements = dialogs.map( dialog => <DialogItem name={dialog.name} id={dialog.id}/>)
+
+    let messages = [
         {id:1, message:'Hi'},
         {id:2, message:'How are you?'},
         {id:3, message:'Text'}
     ]
+    let messagesElements = messages.map(message => <Message message={message.message} id={message.id}/>)
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogItems}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
-                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
-                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
-                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
+                {dialogsElements}
             </div>
             <div className={classes.messages}>
-                <Message message={messagesData[0].message} id={messagesData[0].id}/>
-                <Message message={messagesData[1].message} id={messagesData[1].id}/>
-                <Message message={messagesData[2].message} id={messagesData[2].id}/>
+                {messagesElements}
             </div>
         </div>)
 }
--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -2,10 +2,12 @@ import classes from './MyPosts.module.css';
 import Post from "./Post/Post";
 
 const MyPosts = (props) => {
-    let postData = [
+    let posts = [
         {id:1, message:'Hi, how are you?', like:41},
         {id:2, message:'It is my first post', like:8},
+        {id:3, message:'Test post', like:14},
     ]
+let postsElements = posts.map(post => <Post message={post.message} like={post.like} id={post.id}/>)
     return (
         <div className={classes.postBlock}>
             <h3>My posts</h3>
@@ -15,8 +17,7 @@ const MyPosts = (props) => {
                     <button>Add post</button>
                 </div>
             </div>
-            <Post message={postData[0].message} like={postData[0].like} id={postData[0].id}/>
-            <Post message={postData[1].message} like={postData[1].like} id={postData[1].id}/>
+            {postsElements}
         </div>
     )
 }
