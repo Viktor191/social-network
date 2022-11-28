80aca38a4416f72cca8be1613f57a881fbd4ef23
Viktor <364185@protonmail.com>
2022-11-28 18:41:21 +0400

    L37

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import state, {subscribe, updateNewPostText} from "./redux/state";
-import {addPost} from "./redux/state";
+import store from "./redux/state";
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,12 +12,12 @@ let rerenderEntireTree = (state) => {
     /*const root = ReactDOM.createRoot(document.getElementById('root'));*/
     root.render(
         <React.StrictMode>
-            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+            <App state={state} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)}/>
         </React.StrictMode>
     );
 }
-rerenderEntireTree(state);
-subscribe(rerenderEntireTree);
+rerenderEntireTree(store.getState());
+store.subscribe(rerenderEntireTree);
 
 
 // If you want to start measuring performance in your app, pass a function
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,63 @@
-let rerenderEntireTree = () => {
+
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id:1, message:'Hi, how are you?', like:41},
+                {id:2, message:'It is my first post', like:8},
+                {id:3, message:'Test post', like:14},
+            ],
+            newPostText:'Test text from state 2',
+        },
+        dialogPage: {
+            dialogs: [
+                {id:1, name:'Dimych'},
+                {id:2, name:'Andrey'},
+                {id:3, name:'Sveta'},
+                {id:4, name:'Sasha'},
+                {id:5, name:'Viktor'}
+            ],
+            messages: [
+                {id:1, message:'Hi'},
+                {id:2, message:'How are you?'},
+                {id:3, message:'Text'}
+            ],
+        },
+        sidebar: {
+            friends: [
+                {id:1, name:'Dimych'},
+                {id:2, name:'Andrey'},
+                {id:3, name:'Sveta'},
+            ]
+        },
+    }, /*свойство и его синтаксис*/
+    getState() {
+        return this._state;
+    },
+    _callSubscriber() {
+        console.log()
+    },/* метод и его синтаксис*/
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            like: 0,
+        }
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText) {
+
+        this._state.profilePage.newPostText= newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe (observer) {
+        this._callSubscriber = observer;
+    },
+}
+
+/*let rerenderEntireTree = () => {
     console.log()
 }
 let state = {
@@ -33,7 +92,7 @@ let state = {
     },
 }
 
-window.state = state;
+
 export const addPost = () => {
     let newPost = {
         id: 5,
@@ -51,6 +110,7 @@ export const updateNewPostText = (newText) => {
 }
 export const subscribe = (observer) => {
     rerenderEntireTree = observer;
-}
+}*/
+export default store;
+window.store = store;
 
-export default state;
